6aa5f298fd6ce2b33f797602858aedd98a5c3551
Alan Maia <maiaalan@hotmail.com>
2018-11-08 20:41:27 -0300

    Add files via upload

--- obterEN.py
+++ obterEN.py
@@ -4,25 +4,28 @@ def getNE(yList,uList,n,m):
 	N = len(yList)
 	yList = np.asarray(yList)
 	uList = np.asarray(uList)
-	y = np.zeros((N,n))
-	for i in range(0,N):
+	y = np.zeros((N-1,n))
+	for i in range(0,N-1):
 		for j in range(0,n):
 			if(N-n >= 0):
 				y[i,j] = -yList[i-j]
 			else:
 				y[i,j] = 0
-	u = np.zeros((N,m))
-	for i in range(0,N):
+	u = np.zeros((N-1,m))
+	for i in range(0,N-1):
 		for j in range(0,m):
 			if(N-m >= 0):
 				u[i,j] = -uList[i-j]
 			else:
 				u[i,j] = 0
 	print(y)
+	Y = yList[1:]
+	Y = Y.reshape(N-1,1)
+	print(Y)
 	print(u)			
-	X = np.zeros((N,n+m))
+	X = np.zeros((N-1,n+m))
 	for i in range(0,n+m):
-		for j in range(0,N):
+		for j in range(0,N-1):
 			if i < n:
 				X[j,i] = y[j,0]
 			else:
@@ -30,8 +33,11 @@ def getNE(yList,uList,n,m):
 
 	
 	print(X)
-	teta = np.linalg.inv(np.transpose(X)@X)@np.transpose(X)@y
-
+	print(Y)
+	teta = np.linalg.inv(np.transpose(X)@X)@np.transpose(X)@Y
+	print(teta)
+	print("ERRO:")
+	print(Y-X@teta)
 
 
 
